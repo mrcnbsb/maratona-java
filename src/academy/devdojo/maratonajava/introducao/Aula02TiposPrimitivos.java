ada2cf8abcb5072b6f3606375f05174fc7ebe1c6
mrcnbsb <marconebsb@gmail.com>
2026-04-04 17:43:04 -0300

    Aula02 Tipos Primitivos cont

--- src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
+++ src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -3,7 +3,17 @@ package academy.devdojo.maratonajava.introducao;
 public class Aula02TiposPrimitivos {
     public static void main(String[] args) {
         // int, double, float, char, byte, short, long, boolean
-        int age = 46;
+        byte idadeByte = 46; //1 byte = 8 bits
+        short idadeShort = 46; //2 bytes
+        int age = 46; //4 bytes
+        long numeroGrande = 100000; //8 bytes
+        float salarioFloat = 2500; //4 bytes
+        double salarioDouble = 2000; //8 bytes
+        boolean verdadeiro = true; //1 bit
+        boolean falso = false; // 1 bit
+        char caractere = '\u0041'; // 2 bytes // unicode ou asc
+
         System.out.println("A idade é " + age + " anos");
+        System.out.println(caractere);
     }
 }
