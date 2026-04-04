d39e09007a90fdcf946c9f123035e53c058b8e90
mrcnbsb <marconebsb@gmail.com>
2026-04-04 19:14:18 -0300

    Casting

--- src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
+++ src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -5,15 +5,18 @@ public class Aula02TiposPrimitivos {
         // int, double, float, char, byte, short, long, boolean
         byte idadeByte = 46; //1 byte = 8 bits
         short idadeShort = 46; //2 bytes
-        int age = 46; //4 bytes
+        int idade = (int) 10000000000L; //4 bytes
         long numeroGrande = 100000; //8 bytes
+        long numeroGrande2 = 100000L; //8 bytes
         float salarioFloat = 2500; //4 bytes
-        double salarioDouble = 2000; //8 bytes
+        float salarioFloat2 = 2500.0F; //4 bytes
+        double salarioDouble = 2000.0; //8 bytes
+        double salarioDouble2 = 2000.0D; //8 bytes
         boolean verdadeiro = true; //1 bit
         boolean falso = false; // 1 bit
         char caractere = '\u0041'; // 2 bytes // unicode ou asc
 
-        System.out.println("A idade é " + age + " anos");
-        System.out.println(caractere);
+        System.out.println("A idade é " + idade + " anos");
+        System.out.println(idade);
     }
 }
