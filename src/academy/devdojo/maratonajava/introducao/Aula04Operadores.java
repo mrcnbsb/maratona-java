e147d5ab993fcfd74fb5c6c6917c6698dab46789
mrcnbsb <marconebsb@gmail.com>
2026-04-04 19:44:08 -0300

    Operadores aritmeticos

---
+++ src/academy/devdojo/maratonajava/introducao/Aula04Operadores.java
@@ -0,0 +1,21 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula04Operadores {
+    public static void main(String[] args) {
+        //aritméticos
+        // + - * /
+        int num1 = 10;
+        int num2 = 20;
+
+        //sout+Tab
+        System.out.println(num2 - num1); // + adição
+        System.out.println(num2 + num1); // - subtração
+        System.out.println(num2 * num1); // * multiplicação
+        System.out.println(num2 / num1); // / divisão
+        System.out.println(num1 + num2 + "valor " + num2 + num1); // + concatecação
+
+        double resultado = num1 / num2;
+        System.out.println(resultado);
+
+    }
+}
