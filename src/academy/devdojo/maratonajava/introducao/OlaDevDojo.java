e2278ac1492bbe733c06a3e29c809ddd3c637a4f
mrcnbsb <marconebsb@gmail.com>
2026-04-04 17:17:06 -0300

    Comentarios

--- src/academy/devdojo/maratonajava/introducao/OlaDevDojo.java
+++ src/academy/devdojo/maratonajava/introducao/OlaDevDojo.java
@@ -1,6 +1,14 @@
 package academy.devdojo.maratonajava.introducao;
 
+/**
+ * Isso é um comentário <b>javadoc</b>
+ */
 public class OlaDevDojo {
+    // Isso é um comentário de uma linha
+    /*
+    * Isso é um comentário
+    * de múltiplas linhas
+    * */
     public static void main(String[] args) {
         System.out.println("Ka-ME-HA-ME-HA");
     }
