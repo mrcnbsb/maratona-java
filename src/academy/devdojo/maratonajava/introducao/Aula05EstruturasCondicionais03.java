2010bb61bd2750e93423dbfeaaf6f773787c5e27
mrcnbsb <marconebsb@gmail.com>
2026-04-05 11:34:32 -0300

    Aula05 Estruturas Condicionais Operador Ternário

---
+++ src/academy/devdojo/maratonajava/introducao/Aula05EstruturasCondicionais03.java
@@ -0,0 +1,28 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula05EstruturasCondicionais03 {
+    public static void main(String[] args) {
+
+        // Doar se salario > 5000
+        double salario = 3000;
+        String mensagemDoar = "Eu vou doar 500 pro DevDojo";
+        String mensagemNaoDoar = "Ainda não tenho condições, mas vou ter!";
+        String resultado;
+
+        if(salario > 5000){
+            resultado = mensagemDoar;
+        }else{
+            resultado = mensagemNaoDoar;
+        }
+        System.out.println(resultado);
+
+        // operador ternário
+        // (condicao) ? verdadeiro : falso
+        resultado = (salario > 5000) ? mensagemDoar : mensagemNaoDoar;
+        System.out.println(resultado);
+
+        boolean possoDoar = salario > 5000 ? true : false;
+        System.out.println(possoDoar);
+
+    }
+}
