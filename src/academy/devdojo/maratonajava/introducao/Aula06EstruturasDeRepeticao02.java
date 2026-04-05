03d5104e9989132e1e8f14864d1a6652be962d15
mrcnbsb <marconebsb@gmail.com>
2026-04-05 19:37:53 -0300

    Aula06 Estruturas de Repetição exercício

---
+++ src/academy/devdojo/maratonajava/introducao/Aula06EstruturasDeRepeticao02.java
@@ -0,0 +1,11 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula06EstruturasDeRepeticao02 {
+    public static void main(String[] args) {
+        // Imprima todos os números pares de 0 até 1 milhão
+
+        for(int i = 0; i <= 1000000; i+=2){
+            System.out.println(i);
+        }
+    }
+}
