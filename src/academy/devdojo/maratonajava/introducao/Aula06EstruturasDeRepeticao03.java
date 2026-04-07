7358a5e804717e40929add8bbbb6312e1d2d9586
mrcnbsb <marconebsb@gmail.com>
2026-04-06 21:20:05 -0300

    Aula06 Estruturas de Repetição - break

---
+++ src/academy/devdojo/maratonajava/introducao/Aula06EstruturasDeRepeticao03.java
@@ -0,0 +1,15 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula06EstruturasDeRepeticao03 {
+    public static void main(String[] args) {
+        // Imprima os 25 números de uma contagem até 50
+        int valorMax = 50;
+        for(int i=0; i<=valorMax; i++){
+            System.out.println(i);
+            if(i == 25){
+                break;
+            }
+
+        }
+    }
+}
