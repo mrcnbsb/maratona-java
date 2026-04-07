a4a831485c29e27ec20a873662339b436a2d1c05
mrcnbsb <marconebsb@gmail.com>
2026-04-06 22:29:08 -0300

    Aula08 Arrays Multidimensioanis parte01

---
+++ src/academy/devdojo/maratonajava/introducao/Aula08ArraysMultidimensionais01.java
@@ -0,0 +1,27 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula08ArraysMultidimensionais01 {
+    public static void main(String[] args) {
+        // 1 2 3 4 .. mês
+        // 31 28 31 30 .. dias
+        int[][] dias = new int[3][3];
+        dias[0][0] = 31;
+        dias[0][1] = 28;
+        dias[0][2] = 31;
+
+        dias[1][0] = 30;
+        dias[1][1] = 31;
+        dias[1][2] = 30;
+
+        dias[2][0] = 31;
+        dias[2][1] = 30;
+        dias[2][2] = 31;
+
+        for(int i = 0; i < 3; i++){
+            for(int j = 0; j < 3; j++){
+                System.out.println(dias[i][j]);
+            }
+        }
+
+    }
+}
