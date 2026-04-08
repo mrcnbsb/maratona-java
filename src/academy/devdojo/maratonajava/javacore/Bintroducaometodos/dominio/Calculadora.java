aa10b088a21b56dba4691e65ccde6ae4eb917dcb
mrcnbsb <marconebsb@gmail.com>
2026-04-07 21:30:52 -0300

    Orientação à Objetos - introdução - métodos

---
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Calculadora.java
@@ -0,0 +1,8 @@
+package academy.devdojo.maratonajava.javacore.Bintroducaometodos.dominio;
+
+public class Calculadora {
+
+    public void somarDoisNumeros(){
+        System.out.println(10+10);
+    }
+}
