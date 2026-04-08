4bca84e6b584d4785ab1b3fcdd2c199428d3f80e
mrcnbsb <marconebsb@gmail.com>
2026-04-08 09:23:12 -0300

    Orientação à Objetos - introdução - this

--- src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/test/EstudanteTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/test/EstudanteTest01.java
@@ -3,7 +3,7 @@ package academy.devdojo.maratonajava.javacore.Bintroducaometodos.test;
 import academy.devdojo.maratonajava.javacore.Bintroducaometodos.dominio.Estudante;
 import academy.devdojo.maratonajava.javacore.Bintroducaometodos.dominio.ImpressoraEstudante;
 
-public class EstudanteTest {
+public class EstudanteTest01 {
     public static void main(String[] args) {
         Estudante estudante01 = new Estudante();
         Estudante estudante02 = new Estudante();
