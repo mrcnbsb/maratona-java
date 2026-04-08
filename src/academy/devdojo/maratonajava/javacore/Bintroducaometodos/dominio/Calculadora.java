6974312d88c1d14ce8e6fbf1d5e9f4ee0a399af3
mrcnbsb <marconebsb@gmail.com>
2026-04-07 21:47:44 -0300

    Orientação à Objetos - introdução - métodos parte03

--- src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Calculadora.java
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Calculadora.java
@@ -10,7 +10,14 @@ public class Calculadora {
         System.out.println(21-2);
     }
 
-    public void multiplicarDoisNumeros(int num1, int num2){
+    public void multiplicarDoisNumeros(int num1, float num2){
         System.out.println(num1*num2);
+    }
+
+    public double dividirDoisNumeros(double num1, double num2){
+        if(num2==0){
+            return 0;
+        }
+        return num1/num2;
     }
 }
