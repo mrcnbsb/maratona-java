36e3153f07cf147385ec34f733ec35cd3d4acb5e
mrcnbsb <marconebsb@gmail.com>
2026-04-07 21:21:59 -0300

    Orientação à Objetos - classes e objetos - tipo referência

--- src/academy/devdojo/maratonajava/javacore/Aintroducaoclasses/test/CarroTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Aintroducaoclasses/test/CarroTest01.java
@@ -15,5 +15,13 @@ public class CarroTest01 {
         carro2.ano = 2026;
         carro2.nome = "Dolphin";
         System.out.println("Modelo: " + carro2.modelo + ", ano: " + carro2.ano + ", nome: " + carro2.nome);
+
+        System.out.println("----------");
+        carro2 = carro1;
+        Carro carro3 = carro1;
+
+        System.out.println("Modelo: " + carro1.modelo +  ", ano: " + carro1.ano + ", nome: " + carro1.nome);
+        System.out.println("Modelo: " + carro2.modelo + ", ano: " + carro2.ano + ", nome: " + carro2.nome);
+        System.out.println("Modelo: " + carro3.modelo + ", ano: " + carro3.ano + ", nome: " + carro3.nome);
     }
 }
