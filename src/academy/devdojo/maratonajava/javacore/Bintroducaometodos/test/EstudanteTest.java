26d5f05e928755ade20f7013a83e0d40f6c7750e
mrcnbsb <marconebsb@gmail.com>
2026-04-08 09:07:49 -0300

    Orientação à Objetos - introdução - métodos parte05

---
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/test/EstudanteTest.java
@@ -0,0 +1,24 @@
+package academy.devdojo.maratonajava.javacore.Bintroducaometodos.test;
+
+import academy.devdojo.maratonajava.javacore.Bintroducaometodos.dominio.Estudante;
+import academy.devdojo.maratonajava.javacore.Bintroducaometodos.dominio.ImpressoraEstudante;
+
+public class EstudanteTest {
+    public static void main(String[] args) {
+        Estudante estudante01 = new Estudante();
+        Estudante estudante02 = new Estudante();
+        ImpressoraEstudante impressora =  new ImpressoraEstudante();
+
+        estudante01.nome = "Midoriya";
+        estudante01.sexo = 'M';
+        estudante01.idade = 15;
+
+        estudante02.nome = "Sakura";
+        estudante02.sexo = 'F';
+        estudante02.idade = 16;
+
+        impressora.imprimir(estudante01);
+        impressora.imprimir(estudante02);
+
+    }
+}
