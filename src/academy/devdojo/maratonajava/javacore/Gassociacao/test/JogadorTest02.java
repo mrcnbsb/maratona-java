62d3f61a5ace33bef631ce1e8ea9f9043c6d8d58
mrcnbsb <marconebsb@gmail.com>
2026-04-09 16:54:52 -0300

    Orientação à Objetos - Associação - parte02

---
+++ src/academy/devdojo/maratonajava/javacore/Gassociacao/test/JogadorTest02.java
@@ -0,0 +1,15 @@
+package academy.devdojo.maratonajava.javacore.Gassociacao.test;
+
+import academy.devdojo.maratonajava.javacore.Gassociacao.dominio.Jogador;
+import academy.devdojo.maratonajava.javacore.Gassociacao.dominio.Time;
+
+public class JogadorTest02 {
+    public static void main(String[] args) {
+        Jogador jogador1 = new Jogador("Pelé");
+        jogador1.imprime();
+        Time time = new Time("Seleção Brasileira");
+
+        jogador1.setTime(time);
+        jogador1.imprime();
+    }
+}
