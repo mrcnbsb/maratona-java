9c5fbd35d3c0f36083aabaa39c67b0022230a817
mrcnbsb <marconebsb@gmail.com>
2026-04-09 16:35:20 -0300

    Orientação à Objetos - introdução - bloco de inicialização estático

--- src/academy/devdojo/maratonajava/javacore/Fmodificadorestatico/test/CarroTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Fmodificadorestatico/test/CarroTest01.java
@@ -4,16 +4,16 @@ import academy.devdojo.maratonajava.javacore.Fmodificadorestatico.domain.Carro;
 
 public class CarroTest01 {
     public static void main(String[] args) {
-//        Carro c1 = new Carro("BMW", 280);
-//        Carro c2 = new Carro("Mercedes", 275);
-//        Carro c3 = new Carro("Audi", 290);
+        Carro c1 = new Carro("BMW", 280);
+        Carro c2 = new Carro("Mercedes", 275);
+        Carro c3 = new Carro("Audi", 290);
 
-        System.out.println(Carro.velocidadeLimite);
-        Carro.velocidadeLimite = 180;
-        System.out.println(Carro.velocidadeLimite);
+        System.out.println(Carro.getVelocidadeLimite());
+        Carro.setVelocidadeLimite(180);
+        System.out.println(Carro.getVelocidadeLimite());
 
-//        c1.imprime();
-//        c2.imprime();
-//        c3.imprime();
+        c1.imprime();
+        c2.imprime();
+        c3.imprime();
     }
 }
