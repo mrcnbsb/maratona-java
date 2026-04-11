df267775a3a1e32bcb825a0c63588f771e61548c
mrcnbsb <marconebsb@gmail.com>
2026-04-11 19:05:54 -0300

    Orientação à Objetos - Modificador final

---
+++ src/academy/devdojo/maratonajava/javacore/Jmodificadorfinal/domain/Carro.java
@@ -0,0 +1,17 @@
+package academy.devdojo.maratonajava.javacore.Jmodificadorfinal.domain;
+
+public class Carro {
+    private String nome;
+    public static final double VELOCIDADE_LIMITE = 250;
+
+    public Carro() {
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
