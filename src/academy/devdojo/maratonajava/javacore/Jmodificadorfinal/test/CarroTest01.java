f69d703ef8d028789b8a760fd462ed6b00586d1e
mrcnbsb <marconebsb@gmail.com>
2026-04-11 19:14:33 -0300

    Orientação à Objetos - Modificador Final parte02

--- src/academy/devdojo/maratonajava/javacore/Jmodificadorfinal/test/CarroTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Jmodificadorfinal/test/CarroTest01.java
@@ -1,10 +1,14 @@
 package academy.devdojo.maratonajava.javacore.Jmodificadorfinal.test;
 
 import academy.devdojo.maratonajava.javacore.Jmodificadorfinal.domain.Carro;
+import academy.devdojo.maratonajava.javacore.Jmodificadorfinal.domain.Comprador;
 
 public class CarroTest01 {
     public static void main(String[] args) {
         Carro carro = new Carro();
         System.out.println(Carro.VELOCIDADE_LIMITE);
+        System.out.println(carro.COMPRADOR);
+        carro.COMPRADOR.setNome("Marcone");
+        System.out.println(carro.COMPRADOR);
     }
 }
