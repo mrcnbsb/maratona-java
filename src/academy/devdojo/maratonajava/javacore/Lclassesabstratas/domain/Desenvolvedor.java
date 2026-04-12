d2cbef1edebe5e1ee4cf38cfde212631f6011b06
mrcnbsb <marconebsb@gmail.com>
2026-04-12 18:24:47 -0300

    Orientação à Objetos - Classes Abstratas

---
+++ src/academy/devdojo/maratonajava/javacore/Lclassesabstratas/domain/Desenvolvedor.java
@@ -0,0 +1,16 @@
+package academy.devdojo.maratonajava.javacore.Lclassesabstratas.domain;
+
+public class Desenvolvedor extends Funcionario{
+
+    public Desenvolvedor(String nome, double salario) {
+        super(nome, salario);
+    }
+
+    @Override
+    public String toString() {
+        return "Desenvolvedor{" +
+                "nome='" + nome + '\'' +
+                ", salario=" + salario +
+                '}';
+    }
+}
---
+++ src/academy/devdojo/maratonajava/javacore/Lclassesabstratas/test/FuncionarioTest01.java
@@ -0,0 +1,16 @@
+package academy.devdojo.maratonajava.javacore.Lclassesabstratas.test;
+
+import academy.devdojo.maratonajava.javacore.Lclassesabstratas.domain.Desenvolvedor;
+import academy.devdojo.maratonajava.javacore.Lclassesabstratas.domain.Funcionario;
+import academy.devdojo.maratonajava.javacore.Lclassesabstratas.domain.Gerente;
+
+public class FuncionarioTest01 {
+    public static void main(String[] args) {
+
+        Gerente gerente = new Gerente("Nami", 5000);
+        System.out.println(gerente);
+
+        Desenvolvedor desenvolvedor = new Desenvolvedor("Touya", 12000);
+        System.out.println(desenvolvedor);
+    }
+}
