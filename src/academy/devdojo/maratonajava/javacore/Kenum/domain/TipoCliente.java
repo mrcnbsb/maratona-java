19adf41fa7e255a85987b51ce277ce3ee854ffe2
mrcnbsb <marconebsb@gmail.com>
2026-04-12 13:50:32 -0300

    Orientação à Objetos - Enum - parte02

--- src/academy/devdojo/maratonajava/javacore/Kenum/domain/TipoCliente.java
+++ src/academy/devdojo/maratonajava/javacore/Kenum/domain/TipoCliente.java
@@ -1,6 +1,12 @@
 package academy.devdojo.maratonajava.javacore.Kenum.domain;
 
 public enum TipoCliente {
-    PESSOA_FISICA,
-    PESSOA_JURIDICA
+    PESSOA_FISICA(1),
+    PESSOA_JURIDICA(2);
+
+    public final int VALOR;
+
+    TipoCliente(int valor) {
+        this.VALOR = valor;
+    }
 }
