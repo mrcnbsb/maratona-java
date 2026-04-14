66fc9fd16292f774ccbe7be47b9afa373764edec
mrcnbsb <marconebsb@gmail.com>
2026-04-14 09:51:42 -0300

    Java - Exception - parte03

--- src/academy/devdojo/maratonajava/javacore/Oexception/runtime/test/RutimeExceptionTest02.java
+++ src/academy/devdojo/maratonajava/javacore/Oexception/runtime/test/RutimeExceptionTest02.java
@@ -10,7 +10,13 @@ public class RutimeExceptionTest02 {
         System.out.println("Código finalizado");
     }
 
-    private static int divisao(int a, int b) throws IllegalArgumentException {
+    /**     *
+     * @param a
+     * @param b não pode ser zero
+     * @return
+     * @throws IllegalArgumentException caso b seja zero
+     * */
+    private static int divisao(int a, int b) {
         if(b==0){
             throw new IllegalArgumentException("Argumento ilegal, não pode ser 0");
         }
