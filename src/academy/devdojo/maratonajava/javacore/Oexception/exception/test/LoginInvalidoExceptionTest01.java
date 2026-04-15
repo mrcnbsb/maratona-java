cc4126cf4f75e2a6f9def0acfe8f4d7657a851a9
mrcnbsb <marconebsb@gmail.com>
2026-04-15 08:46:26 -0300

    Java - Exception - Exceções Customizadas

---
+++ src/academy/devdojo/maratonajava/javacore/Oexception/exception/test/LoginInvalidoExceptionTest01.java
@@ -0,0 +1,29 @@
+package academy.devdojo.maratonajava.javacore.Oexception.exception.test;
+
+import academy.devdojo.maratonajava.javacore.Oexception.exception.domain.LoginInvalidoException;
+
+import java.util.Scanner;
+
+public class LoginInvalidoExceptionTest01 {
+    public static void main(String[] args) {
+        try {
+            logar();
+        } catch (LoginInvalidoException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void logar() throws LoginInvalidoException {
+        Scanner teclado = new Scanner(System.in);
+        String usernameDB = "Goku";
+        String senhaDB = "ssj";
+        System.out.println("Usuario");
+        String usernameDigitado = teclado.nextLine();
+        System.out.println("Senha");
+        String senhaDigitada = teclado.nextLine();
+        if(!usernameDB.equals(usernameDigitado) || !senhaDB.equals(senhaDigitada)){
+            throw new LoginInvalidoException("Usuário ou senha inválidos");
+        }
+        System.out.println("Login realizado com sucesso");
+    }
+}
