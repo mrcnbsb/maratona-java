37c557d491cd17f0dd87415bd27dcaa1a409d346
mrcnbsb <marconebsb@gmail.com>
2026-04-15 09:39:05 -0300

    Java - Classes Utilitárias - Wrappers

---
+++ src/academy/devdojo/maratonajava/javacore/Pwrappers/test/WrapperTest01.java
@@ -0,0 +1,25 @@
+package academy.devdojo.maratonajava.javacore.Pwrappers.test;
+
+public class WrapperTest01 {
+    public static void main(String[] args) {
+        byte bytep = 1;
+        short shortp = 1;
+        int intp = 1;
+        long longp = 10L;
+        float floatp = 10F;
+        double doublep = 10D;
+        char charp = 'M';
+        boolean boolp = false;
+
+        Byte byteW = 1;
+        Short shortW = 1;
+        Integer intW = 1;
+        Long longW = 10L;
+        Float floatW = 10F;
+        Double doubleW = 10D;
+        Character charW = 'M';
+        Boolean boolW = false;
+
+        System.out.println(byteW.);
+    }
+}
