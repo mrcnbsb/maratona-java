d3d076edf7c267daadab518ac400a010686c0b36
mrcnbsb <marconebsb@gmail.com>
2026-04-15 08:53:46 -0300

    Java - Exception - regras de sobrescrita

---
+++ src/academy/devdojo/maratonajava/javacore/Oexception/exception/domain/Funcionario.java
@@ -0,0 +1,7 @@
+package academy.devdojo.maratonajava.javacore.Oexception.exception.domain;
+
+public class Funcionario extends Pessoa{
+    public void salvar() throws LoginInvalidoException{
+        System.out.println("Salvando funcionario com sucesso");
+    }
+}
