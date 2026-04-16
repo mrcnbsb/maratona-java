86b3d0d0550b30ce960c6b452daa9123204aefc8
mrcnbsb <marconebsb@gmail.com>
2026-04-15 21:17:12 -0300

    Java - Classes Utilitárias - String

---
+++ src/academy/devdojo/maratonajava/javacore/Qstring/test/StringTest01.java
@@ -0,0 +1,15 @@
+package academy.devdojo.maratonajava.javacore.Qstring.test;
+
+public class StringTest01 {
+    public static void main(String[] args) {
+        //Strings são imutáveis
+        String nome = "Marcone"; //String constant pool
+        String nome2 = "Marcone";
+        nome = nome.concat(" Suane");
+        System.out.println(nome);
+        System.out.println(nome==nome2);
+        String nome3 = new String("Marcone"); // 1 variável de referência, 2 objeto do tipo string, 3 uma string no pool de string
+        System.out.println(nome2 == nome3.intern());
+        System.out.println(nome3);
+    }
+}
