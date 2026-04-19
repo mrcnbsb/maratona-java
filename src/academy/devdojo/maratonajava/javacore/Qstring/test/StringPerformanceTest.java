39750dea10f4857a599cc2f162e4164304994a0a
mrcnbsb <marconebsb@gmail.com>
2026-04-19 11:41:40 -0300

    Java - String - Performance

---
+++ src/academy/devdojo/maratonajava/javacore/Qstring/test/StringPerformanceTest.java
@@ -0,0 +1,41 @@
+package academy.devdojo.maratonajava.javacore.Qstring.test;
+
+public class StringPerformanceTest {
+    public static void main(String[] args) {
+        long inicio = System.currentTimeMillis();
+        concatString(1_000_000);
+        long fim = System.currentTimeMillis();
+        System.out.println("Tempo gasto para String: " + (fim - inicio) + "ms");
+
+        inicio = System.currentTimeMillis();
+        concatStringBuilder(1_000_000);
+        fim = System.currentTimeMillis();
+        System.out.println("Tempo gasto para StringBuilder: " + (fim - inicio) + "ms");
+
+        inicio = System.currentTimeMillis();
+        concatStringBuffer(1_000_000);
+        fim = System.currentTimeMillis();
+        System.out.println("Tempo gasto para StringBuffer: " + (fim - inicio) + "ms");
+    }
+
+    private static void concatStringBuffer(int tamanho){
+        StringBuffer sbf = new StringBuffer(tamanho);
+        for (int i = 0; i < tamanho ; i++) {
+            sbf.append(1); //0,01,012,0123
+        }
+    }
+
+    private static void concatStringBuilder(int tamanho){
+        StringBuilder sb = new StringBuilder(tamanho);
+        for (int i = 0; i < tamanho ; i++) {
+            sb.append(1); //0,01,012,0123
+        }
+    }
+
+    private static void concatString(int tamanho){
+        String texto = "";
+        for (int i = 0; i < tamanho ; i++) {
+            texto += i; //0,01,012,0123
+        }
+    }
+}
