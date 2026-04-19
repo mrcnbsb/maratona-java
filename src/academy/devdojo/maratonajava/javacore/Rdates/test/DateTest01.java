af226cef48737aa9f468537357bd89d757d9f6cb
mrcnbsb <marconebsb@gmail.com>
2026-04-19 12:05:49 -0300

    Java - Datas - classe Date

---
+++ src/academy/devdojo/maratonajava/javacore/Rdates/test/DateTest01.java
@@ -0,0 +1,16 @@
+package academy.devdojo.maratonajava.javacore.Rdates.test;
+
+import java.util.Date;
+
+public class DateTest01 {
+    public static void main(String[] args) {
+        // Date -> sistemas legados
+        Date date =  new Date(); // long 1000000
+        System.out.println(date);
+        System.out.println(date.getTime());
+        date.setTime(date.getTime()+3_600_000);
+        System.out.println(date);
+
+
+    }
+}
