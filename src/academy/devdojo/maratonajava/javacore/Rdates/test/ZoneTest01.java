facb15aaa94cf2eafa8a73afe1255de865b003ef
mrcnbsb <marconebsb@gmail.com>
2026-04-22 10:53:37 -0300

    Java - datas - ZoneIdTime, ZoneId, OffSetDateTime

---
+++ src/academy/devdojo/maratonajava/javacore/Rdates/test/ZoneTest01.java
@@ -0,0 +1,30 @@
+package academy.devdojo.maratonajava.javacore.Rdates.test;
+
+import java.time.*;
+import java.util.Map;
+
+public class ZoneTest01 {
+    public static void main(String[] args) {
+        Map<String, String> shortIds = ZoneId.SHORT_IDS;
+        System.out.println(shortIds);
+        System.out.println(ZoneId.systemDefault());
+        ZoneId tokyoZone = ZoneId.of("Asia/Tokyo");
+        LocalDateTime now = LocalDateTime.now();
+        System.out.println(now);
+
+        ZonedDateTime zonedDateTime = now.atZone(tokyoZone);
+        System.out.println(zonedDateTime);
+
+        Instant nowInstant = Instant.now();
+        System.out.println(nowInstant);
+        ZonedDateTime zonedDateTime2 = nowInstant.atZone(tokyoZone);
+        System.out.println(zonedDateTime2);
+
+        System.out.println(ZoneOffset.MIN);
+        System.out.println(ZoneOffset.MAX);
+
+        ZoneOffset offsetManaus = ZoneOffset.of("-04:00");
+        OffsetDateTime offsetDateTime =  now.atOffset(offsetManaus);
+        System.out.println(offsetDateTime);
+    }
+}
