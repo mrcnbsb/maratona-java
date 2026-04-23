78ec91d22eeedf81e48c491d537645b6d05eab22
mrcnbsb <marconebsb@gmail.com>
2026-04-23 10:45:45 -0300

    Regex - Metacaracteres

---
+++ src/academy/devdojo/maratonajava/javacore/Uregex/test/PatternMatcherTest02.java
@@ -0,0 +1,28 @@
+package academy.devdojo.maratonajava.javacore.Uregex.test;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class PatternMatcherTest02 {
+    public static void main(String[] args) {
+        // expressões regulares
+        // \d = Todos os dígitos
+        // \D = Tudo que não for dígito
+        // \s = Todos os espaços em branco \t \n \f r
+        // \S = Todos os caracteres excluindo os brancos
+        // \w = a-z, A-Z, dígitos, _
+        // \W = Tudo que não for incluso no \w
+        String regex = "\\W";
+        String texto = "@#hh_j2 1\t2gvh21";
+
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(texto);
+        System.out.println("texto:  " + texto);
+        System.out.println("indice: 0123456789");
+        System.out.println("regex: " + regex);
+        System.out.println("Posicoes encontradas");
+        while (matcher.find()) {
+            System.out.print(matcher.start() + " " + matcher.group()+"\n");
+        }
+    }
+}
