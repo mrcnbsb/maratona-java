dff841f37e5b909c566d9043ca2dbee635f50ee7
mrcnbsb <marconebsb@gmail.com>
2026-05-24 20:43:48 -0300

    Java Generics pt 02

---
+++ src/academy/devdojo/maratonajava/javacore/Zgenerics/test/WildcardTest02.java
@@ -0,0 +1,21 @@
+package academy.devdojo.maratonajava.javacore.Zgenerics.test;
+
+import java.util.List;
+
+public class WildcardTest02 {
+    public static void main(String[] args) {
+        List<Cachorro> cachorros = List.of(new Cachorro(), new Cachorro());
+        List<Gato> gatos = List.of(new Gato(), new Gato());
+        printConsulta(cachorros);
+        printConsulta(gatos);
+    }
+
+    // Type erasure
+    private static void printConsulta(List<Animal> animals){
+        for (Animal animal : animals){
+            animal.consulta();
+        }
+
+
+    }
+}
