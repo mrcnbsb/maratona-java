ca23633e545504858578bf89b7fc92da0f9719fe
mrcnbsb <marconebsb@gmail.com>
2026-05-24 20:53:55 -0300

    Java Wildcards

--- src/academy/devdojo/maratonajava/javacore/Zgenerics/test/WildcardTest02.java
+++ src/academy/devdojo/maratonajava/javacore/Zgenerics/test/WildcardTest02.java
@@ -6,8 +6,8 @@ public class WildcardTest02 {
     public static void main(String[] args) {
         List<Cachorro> cachorros = List.of(new Cachorro(), new Cachorro());
         List<Gato> gatos = List.of(new Gato(), new Gato());
-        printConsulta(cachorros);
-        printConsulta(gatos);
+//        printConsulta(cachorros);
+//        printConsulta(gatos);
     }
 
     // Type erasure
